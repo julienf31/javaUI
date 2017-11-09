3b30e63378ecb1b920399f909f0c41c06749c234
julienf31 <julien.fournier@ynov.com>
2017-11-09 13:55:49 +0100

    mvc

---
+++ src/gui/components/DashboardListButton.java
@@ -0,0 +1,9 @@
+package gui.components;
+
+import javax.swing.*;
+
+public class DashboardListButton extends JButton {
+    public DashboardListButton() {
+        this.setText("Liste des employés");
+    }
+}
